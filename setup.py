823e698b4858f4cc24a82cc5b6984184240ae80a
Dmitry <root@uralbash.ru>
2014-09-16 19:17:34 +0600

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,46 +1,5 @@
-import os
-import glob
-import fnmatch
 from setuptools import setup
 
-
-def opj(*args):
-    path = os.path.join(*args)
-    return os.path.normpath(path)
-
-badnames = [".pyc", ".py", "~", "no_"]
-
-
-def find_data_files(srcdir, *wildcards, **kw):
-    # get a list of all files under the srcdir matching wildcards,
-    # returned in a format to be used for install_data
-    def walk_helper(arg, dirname, files):
-        if '.svn' in dirname:
-            return
-        names = []
-        lst, wildcards = arg
-        for wc in wildcards:
-            wc_name = opj(dirname, wc)
-            for f in files:
-                filename = opj(dirname, f)
-
-                if not any(bad in filename for bad in badnames):
-                    if fnmatch.fnmatch(filename, wc_name)\
-                            and not os.path.isdir(filename):
-                        names.append(filename)
-        if names:
-            lst.append((dirname, names))
-
-    file_list = []
-    recursive = kw.get('recursive', True)
-    if recursive:
-        os.walk(srcdir, walk_helper, (file_list, wildcards))
-    else:
-        walk_helper((file_list, wildcards),
-                    srcdir,
-                    [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
-    return file_list
-files = find_data_files('pyramid_sacrud/', '*.*')
 
 setup(
     name='pyramid_sacrud',
@@ -51,7 +10,6 @@ setup(
 
     packages=['pyramid_sacrud', 'pyramid_sacrud.common',
               'pyramid_sacrud.views'],
-    data_files=files,
     include_package_data=True,
     zip_safe=False,
     test_suite="nose.collector",
