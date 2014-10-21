c95d41b307f6811600f57f8712ad86736dfb7303
Dmitry <root@uralbash.ru>
2014-10-21 15:50:29 +0600

    move webassets to gulp

--- pyramid_sacrud/assets.py
+++ pyramid_sacrud/assets.py
@@ -11,50 +11,13 @@ Assets
 import os
 from shutil import copyfile
 
-from webassets import Bundle
-
-
-def webassets_init(config):
-    curdir = os.path.dirname(os.path.abspath(__file__))
-    settings = config.registry.settings
-    settings["webassets.debug"] = "True"
-    settings["webassets.base_dir"] = os.path.join(curdir, 'static')
-    settings["webassets.base_url"] = "/%s/sa_static" % config.route_prefix
-    settings["webassets.updater"] = "always"
-    settings["webassets.jst_compiler"] = "Handlebars.compile"
-    settings["webassets.url_expire"] = "False"
-    settings["webassets.static_view"] = "True"
-    settings["webassets.cache_max_age"] = 3600
-
-    config.include('pyramid_webassets')
-
-    config.add_jinja2_extension('webassets.ext.jinja2.AssetsExtension')
-    assets_env = config.get_webassets_env()
-    jinja2_env = config.get_jinja2_environment()
-    jinja2_env.assets_environment = assets_env
-
-
-def add_css_assets(config):
-    settings = config.registry.settings
-    css_file = os.path.join(settings["webassets.base_dir"],
-                            'css', '__main.css')
-    debug_css = settings.get('sacrud.debug_css', False)
-    if debug_css == "True":
-        bundle = Bundle('css/*.css',
-                        'css/**/*.css',
-                        filters='cssmin',
-                        output=css_file)
-        env = config.get_webassets_env()
-        bundle._set_env(env)
-        bundle.build()
-    config.add_webasset('sa_css', css_file)
-
 
 def add_js_assets(config):                                  # pragma: no cover
     settings = config.registry.settings
     if not settings.get('sacrud.debug_js', False):
         return
-    js_folder = os.path.join(settings["webassets.base_dir"], 'js')
+    here = os.path.dirname(__file__)
+    js_folder = os.path.join(here, 'static', 'js')
 
     bower = ["jquery/dist/jquery.min.js",
              "chosen/public/chosen.jquery.min.js",
@@ -76,6 +39,4 @@ def add_js_assets(config):                                  # pragma: no cover
 
 
 def includeme(config):
-    config.include(webassets_init)
-    config.include(add_css_assets)
     config.include(add_js_assets)
