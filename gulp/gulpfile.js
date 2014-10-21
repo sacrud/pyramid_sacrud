ec2fc62a6c6c345223d21179c9fcf1c8b3357264
Arkadiy Zamaraev <arkadiy@bk.ru>
2014-10-21 15:38:35 +0600

    gulp on

---
+++ gulp/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat');
+    watch = require('gulp-watch');
+
+var staticPath = '../pyramid_sacrud/static/';
+    
+gulp.task('concat', function() {
+    gulp.src([
+        staticPath + 'css/*.css',
+        staticPath + 'css/**/*.css',
+        '!' + staticPath + 'css/__main.css'
+        ])
+        .pipe(concat('__main.css'))
+        .pipe(gulp.dest(staticPath + 'css/'));
+});
+
+gulp.task('watch', function () {
+    watch([staticPath + 'css/*.css', staticPath + 'css/**/*.css',],
+        function (files, cb) {
+            gulp.start('concat', cb);
+        });
+});
