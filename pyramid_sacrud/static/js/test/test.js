a73c3ad429cd47d35480109c9211baa91df3bd36
Arkadiy Zamaraev <arkadiy@bk.ru>
2014-11-19 16:42:51 +0500

    add selenium test add chai and webdriver to package json

---
+++ pyramid_sacrud/static/js/test/test.js
@@ -0,0 +1,46 @@
+var webdriver = require('selenium-webdriver');
+
+var URL = 'http://127.0.0.1:8000/login/';
+
+var driver = new webdriver.Builder()
+    .withCapabilities({ 'browserName': 'chrome' })
+    .build();
+
+var chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect;
+
+describe('SACRUD Tests', function() {
+    before(function(done) {
+        driver.get(URL).then(function(){ done(); });
+    });
+
+    after(function(done) {
+        driver.quit().then(function(){ done();});
+    });
+
+    it('Login to SACRUD', function() {
+        var username = driver.findElement(webdriver.By.name('login'));
+        username.sendKeys('admin');
+        username.getAttribute('value').then(function(value) {
+            assert.equal(value, 'admin');
+        });
+        var password = driver.findElement(webdriver.By.name('password'));
+        password.sendKeys('123');
+        password.getAttribute('value').then(function(value) {
+            assert.equal(value, '123');
+        });
+        var loginButton = driver.findElement(webdriver.By.name('submit'));
+            loginButton.click();
+    });
+
+    it('Go to SACRUD!', function(){
+        var goButton = driver.findElement(webdriver.By.name('goToSACRUD'));
+            goButton.click();
+    });
+
+    it('Logout to SACRUD', function(){
+        var logout = driver.findElement(webdriver.By.name('logoutLink'));
+            logout.click();
+    });
+});
