f7025125344b1c62b4fa91cc7c75510a546fab84
nikita4477 <ggift@mail.ru>
2014-12-11 17:16:17 +0500

    clean travis.yml

--- travis_build_pse.sh
+++ travis_build_pse.sh
@@ -2,6 +2,4 @@
 # Get last child project build number
 BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/ITCase/pyramid_sacrud_example/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 # Restart last child project build
-echo $BUILD_NUM
-echo $AUTH_TOKEN
-echo $(curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN)
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
