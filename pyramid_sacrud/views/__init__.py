896a97508dcf3a1a259a46a236def5db9e8f5237
Dmitry <root@uralbash.ru>
2015-01-13 19:18:40 +0500

    New format of settings and home page generator widget #67

--- pyramid_sacrud/views/__init__.py
+++ pyramid_sacrud/views/__init__.py
@@ -9,34 +9,15 @@
 """
 Views for Pyramid frontend
 """
+from collections import OrderedDict
 from pyramid.view import view_config
 
 from ..common import get_settings_param
 
 
-def sorted_dashboard_widget(tables):
-    class DashboardWidget(object):
-        def __init__(self, name, **kwargs):
-            self.name = name
-            self.tables = kwargs['tables']
-            self.position = kwargs['position']
-    widgets = []
-    for k, v in tables.items():
-        widgets.append(DashboardWidget(name=k, **v))
-        widgets = sorted(widgets, key=lambda x: x.position)
-    for i, widget in enumerate(widgets[:-1]):
-        delta = widgets[i+1].position - widget.position
-        for x in range(delta-1):
-            widgets.insert(i+1, DashboardWidget('', tables=[], position=i+x+2))
-    return widgets
-
-
 @view_config(route_name='sa_home', renderer='/sacrud/home.jinja2',
              permission='pyramid_sacrud_home')
 def sa_home(request):
-    tables = get_settings_param(request, 'pyramid_sacrud.models')
-    dashboard_columns = request.registry.settings\
-        .get('sacrud_dashboard_columns', 3)
-    return {'dashboard_columns': dashboard_columns,
-            'widgets': sorted_dashboard_widget(tables)
-            }
+    tables = OrderedDict(get_settings_param(request, 'pyramid_sacrud.models'))
+    dashboard_row_len = get_settings_param(request, 'sacrud_dashboard_row_len')
+    return {'dashboard_row_len': dashboard_row_len or 3, 'tables': tables}
