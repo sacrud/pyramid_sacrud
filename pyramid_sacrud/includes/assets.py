912bea833cecd3d565b3e5b0cde5ab51f49b532b
Dmitry <root@uralbash.ru>
2015-03-03 16:15:58 +0500

    fix error with jinja2 Environment

--- pyramid_sacrud/includes/assets.py
+++ pyramid_sacrud/includes/assets.py
@@ -11,17 +11,15 @@ Assets
 from ..common import _silent_none
 
 
-def add_jinja2(config):
-    config.include('pyramid_jinja2')
+def add_jinja2_silent_none(config):  # pragma: no cover
     config.commit()
     jinja2_env = config.get_jinja2_environment()
     jinja2_env.finalize = _silent_none
-    config.add_jinja2_search_path("pyramid_sacrud:templates")
-    config.add_jinja2_extension('jinja2.ext.loopcontrols')
 
 
 def includeme(config):
-    config.include(add_jinja2)
+    config.include('pyramid_jinja2')
+    config.add_jinja2_search_path("pyramid_sacrud:templates")
     config.include('sacrud_deform')
     config.add_static_view('deform_static', 'deform:static')
     config.add_static_view('sa_static', 'pyramid_sacrud:static')
