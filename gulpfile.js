e45001fd9d88353c9407d2ee66e282a4867521d9
Arkadiy Zamaraev <arkadiy@bk.ru>
2015-03-13 15:04:19 +0500

    closes #72 closes #71 closes #69

--- gulpfile.js
+++ gulpfile.js
@@ -1,123 +1,44 @@
-var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    clean = require('gulp-clean'),
+var autoprefixer = require('gulp-autoprefixer'),
+    batch = require('gulp-batch'),
     concat = require('gulp-concat'),
-    filter = require('gulp-filter'),
-    minifyCSS = require('gulp-minify-css'),
+    gulp = require('gulp'),
     gutil = require('gulp-util'),
+    minifyCSS = require('gulp-minify-css'),
+    newer = require('gulp-newer'),
+    sourcemaps = require('gulp-sourcemaps'),
     watch = require('gulp-watch');
 
-var _ = require("underscore"),
-    browserify = require('browserify'),
-    glob = require("glob"),
-    mainBowerFiles = require('main-bower-files'),
-    minimatch = require("minimatch"),
-    path = require('path'),
-    map = require('vinyl-map'),
-    source = require('vinyl-source-stream');
+var map = require('vinyl-map');
 
-function getFiles(path, type) {
-
-    var files = glob.sync(path + '**/*.' + type);
-    target = minimatch.match(files, '__*.' + type, { matchBase: true });
-    ignore = _.map(target, function(item){ return '!' + item; });
-    result = files.concat(ignore);
-
-    if(type === undefined) { gutil.log(gutil.colors.red('Failed getFiles'), gutil.colors.yellow('type === undefined')); }
-    if(files.length === 0) { gutil.log(gutil.colors.red('Failed getFiles'), gutil.colors.yellow('files not found, check path')); }
-
-    return result;
-}
-
-// get css and js files from bower components
-var css_path = glob.sync('./*/static/css/');
-var js_path = glob.sync('./*/static/js/');
-var components = glob.sync('./bower_components/*').map(function(componentDir) {
-        return path.basename(componentDir);
-    });
-
-components.forEach(function(name) {
-
-    gulp.task(name+'-clean', function(done) {
-        // gulp.src(js_path + '/lib/' + name + '/').pipe(clean());
-        gulp.src(css_path + '/lib/' + name + '/').pipe(clean()).on('end', done);
-    });
-
-    // copy css files from bower component to static dir in project
-    gulp.task(name+'-css', function(done) {
-        gulp.src(mainBowerFiles('**/' + name + '/**'))
-            .pipe(filter('*.css'))
-            .pipe(gulp.dest(css_path + '/lib/' + name + '/'))
-            .pipe(map(function(code, filename) { gutil.log('Copy ' + gutil.colors.green(filename)); }))
-            .on('end', done);
-    });
-
-    // copy js files from bower component to static dir in project
-    gulp.task(name+'-js', function(done) {
-        gulp.src(mainBowerFiles('**/' + name + '/**'))
-            .pipe(filter('*.js'))
-            .pipe(gulp.dest(js_path + '/lib/' + name + '/'))
-            .pipe(map(function(code, filename) { gutil.log('Copy ' + gutil.colors.green(filename)); }))
-            .on('end', done);
-    });
-
-    // build component by name
-    gulp.task(name+'-build', [name+'-clean', name+'-css', name+'-js']);
-});
-
-// build all components
-gulp.task('build-components', components.map(function(name){ return name+'-build'; }));
-
-
-gulp.task('css', ['pickadate-clean', 'pickadate-css'], function() {
-    var path = glob.sync('./*/static/css/'),
-        concatFiles = getFiles(path ,'css');
-
-    gulp.src(concatFiles)
+gulp.task('css', function() {
+    patterns = ['./pyramid_sacrud/static/css/*.css', './pyramid_sacrud/static/css/**/*.css',
+                '!./pyramid_sacrud/static/css/__sacrud.css'];
+    gulp.src(patterns)
+        .pipe(newer('./pyramid_sacrud/static/css/__sacrud.css'))
+        .pipe(sourcemaps.init())
         .pipe(autoprefixer({
-            browsers: [
-                'Firefox >= 3',
-                'Explorer >= 6',
-                'Opera >= 9',
-                'Chrome >= 15',
-                'Safari >= 4',
-                '> 1%'
-            ],
+            browsers: ['Firefox >= 3', 'Explorer >= 6', 'Opera >= 9', 'Chrome >= 15', 'Safari >= 4', '> 1%'],
             cascade: false
         }))
-        .on('error', function(err){
-            gutil.log(gutil.colors.red('Failed to autoprefixer'), gutil.colors.yellow(err.message));
+        .on('error', function(err) {
+            gutil.log(gutil.colors.red('Autoprefixer Error'), gutil.colors.yellow(err.message));
         })
         .pipe(minifyCSS())
         .pipe(concat('__sacrud.css'))
-        .pipe(gulp.dest(path + '/'));
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./pyramid_sacrud/static/css/'))
+        .pipe(map(function(code, filename) { gutil.log('CSS ' + gutil.colors.green(filename)); }))
+        .on('error', gutil.log);
+
 });
 
-gulp.task('browserify', function() {
-    browserify('./pyramid_sacrud/static/js/main.js', { debug: false })
-        .bundle()
-        .on('error', function (err) {
-            gutil.log(gutil.colors.red('Failed to browserify'), gutil.colors.yellow(err.message));
-        })
-        .pipe(source('__main.js'))
-        .pipe(gulp.dest('./pyramid_sacrud/static/js/'));
+
+gulp.task('watch', function() {
+    watch(['./pyramid_sacrud/static/css/*.css', './pyramid_sacrud/static/css/**/*.css',
+           '!./pyramid_sacrud/static/css/__sacrud.css'], batch(function () {
+        gulp.start('css');
+        cb();
+    }));
 });
 
-gulp.task('watch', function () {
-
-    // var pathCSS = glob.sync('./*/static/css/'),
-    //     watchCSSFiles = getFiles(pathCSS, 'css');
-
-    var pathJS = glob.sync('./*/static/js/'),
-        watchJSFiles = getFiles(pathJS, 'js');
-
-    // watch(watchCSSFiles, function (files, cb) {
-    //     gulp.start('css', cb);
-    // });
-
-    watch(watchJSFiles, function (files, cb) {
-        gulp.start('browserify', cb);
-    });
-});
-
-gulp.task('default', ['browserify']);
+gulp.task('default', ['watch']);
