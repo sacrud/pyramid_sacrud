1c529813ea4aa071f3dca8f00e55d5e3ec6a3232
Dmitry <root@uralbash.ru>
2015-05-30 18:33:00 +0500

    fix js depencdiens

--- pyramid_sacrud/static/js/__pyramid_sacrud.js
+++ pyramid_sacrud/static/js/__pyramid_sacrud.js
@@ -4,10 +4,10 @@ require('pickadate');
 require('pickatime');
 
 var options = require('./app/options.js');
-// var popup = require('./app/popup.js').Popup(options.popup, options);
+var popup = require('./app/popup.js').Popup(options.popup, options);
 var selectable = require('./app/selectable.js').SelectableTable('table > tbody', options);
 
-},{"./app/options.js":5,"./app/selectable.js":6,"pickadate":2,"pickatime":4,"picker":3}],2:[function(require,module,exports){
+},{"./app/options.js":5,"./app/popup.js":6,"./app/selectable.js":7,"pickadate":2,"pickatime":4,"picker":3}],2:[function(require,module,exports){
 (function (global){
 ; var __browserify_shim_require__=require;(function browserifyShim(module, exports, require, define, browserify_shim__define__module__export__) {
 /*!
@@ -3583,6 +3583,74 @@ module.exports = {
 };
 
 },{}],6:[function(require,module,exports){
+var Popup = function (el, options) {
+  if (!(this instanceof Popup)) {
+    return new Popup(el, options);
+  }
+  this.el = $(el);
+  this.options = options;
+  this._bindEvents();
+};
+
+Popup.prototype._bindEvents = function() {
+  $(document).on('click', 'div.'+this.options.state_disable_class, this.blockDisabledButton.bind(this));
+  $(document).on('click', this.options.div_delete_button, this.showDeletePopup.bind(this));
+  $(document).on('click', this.options.popup_close_button, this.hidePopup.bind(this));
+  $(document).on('click', this.options.popup_main_button, this.checkButton.bind(this));
+};
+
+// $(document).on('click', 'div.popup', function function_name (argument) {
+//     console.log('yippee-ki-yay motherf*cker');
+// });
+
+Popup.prototype.blockDisabledButton = function (evnt) {
+  evnt.stopImmediatePropagation();
+};
+
+Popup.prototype.showDeletePopup = function (evnt) {
+  this.el.css("display", "table");
+  $('.popup-inner__content-delete').show();
+  $('.popup-inner__content-elfinder').hide();
+  // this.showDeletePopupContent();
+};
+
+Popup.prototype.hidePopup = function (evnt) {
+  this.el.hide();
+  $('.popup-inner__content-delete').hide();
+  $('.popup-inner__content-elfinder').hide();
+  if ($(evnt.currentTarget).attr('href') !== undefined) {
+    evnt.preventDefault();
+  }
+  // this.hidePopupContent();
+};
+
+// Popup.prototype.showDeletePopupContent = function (evnt) {};
+// Popup.prototype.hidePopupContent = function (evnt) {};
+
+Popup.prototype.checkButton = function (evnt) {
+  var status = $(evnt.currentTarget).data('status');
+  if ((status === undefined) || (status == 'cancel')) {
+    this.hidePopup(evnt);
+  } else if ($(this.options.sacrud_form).length) {  // if (typeof options != "undefined")
+    this._formSubmit($(this.options.sacrud_form), status);
+  }
+};
+
+// Used in list template
+Popup.prototype._formSubmit = function (form, status) {
+  // for delete object, need to send status 'delete'
+  $(this.options.input_selected_action).val(status);
+  form.submit();
+};
+
+// Main entry point
+module.exports = function popup(el, options) {
+  return new Popup(el, options);
+};
+
+module.exports.Popup = Popup;
+
+},{}],7:[function(require,module,exports){
 var rows_state_unselecting, current_rows, first_selected_row, global_options;
 
 var SelectableTable = function (el, options) {
