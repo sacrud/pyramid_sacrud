bdc44fd96ad55964ec4b1ebc7f1e7dabac93874c
Dmitry <root@uralbash.ru>
2015-06-03 13:10:33 +0500

    clean up

--- pyramid_sacrud/tests/features/environment.py
+++ pyramid_sacrud/tests/features/environment.py
@@ -1,21 +1,11 @@
 import imp
 
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 imp.load_source('pyramid_sacrud_example', 'example/pyramid_sacrud_example.py')
 from pyramid_sacrud_example import User
-
-
-user_agent = (
-    "Mozilla/28.0 (Macintosh; Intel Mac OS X 10_8_4) " +
-    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36"
-)
-
-dcap = dict(DesiredCapabilities.PHANTOMJS)
-dcap["phantomjs.page.settings.userAgent"] = user_agent
 
 
 def before_all(context):
