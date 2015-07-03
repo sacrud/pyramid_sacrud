d9571fa3462e2b2798f6f5bee18ebc2ce103a593
Dmitry <root@uralbash.ru>
2015-07-03 20:11:30 +0500

    clean example

--- example/setup.py
+++ example/setup.py
@@ -1,11 +1,20 @@
+import os
 from setuptools import setup
 
 version = '1.0'
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+
+def read(name):
+    with open(os.path.join(here, name)) as f:
+        return f.read()
 
 setup(
     name='pyramid_sacrud_example',
     version=version,
     py_modules=['pyramid_sacrud_example'],
+    install_requires=read('requirements.txt'),
     entry_points="""
 [paste.app_factory]
 main = pyramid_sacrud_example:main
