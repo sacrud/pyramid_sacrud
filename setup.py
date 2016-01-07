4652c96a426e62e439311af4f7e885a3f061d87b
Dmitry <root@uralbash.ru>
2016-01-07 22:41:45 +0500

    Final 0.2.2 version with old architecture!

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(name):
 
 setup(
     name='pyramid_sacrud',
-    version="0.2.1",
+    version="0.2.2",
     url='http://github.com/ITCase/pyramid_sacrud/',
     author='Svintsov Dmitry',
     author_email='sacrud@uralbash.ru',
