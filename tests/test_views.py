1e59fd9e26b7f59b0e4f2ddccc680715db7de41e
Svintsov Dmitry <root@uralbash.ru>
2016-05-04 19:19:48 +0500

    Added test for unicode group name #122

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,4 +1,10 @@
-# from collections import OrderedDict
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 uralbash <root@uralbash.ru>
+#
+# Distributed under terms of the MIT license.
 
 from pyramid import testing
 from pyramid_sacrud import CONFIG_RESOURCES
@@ -34,6 +40,20 @@ class TestHome(object):
         request.registry.settings = {CONFIG_RESOURCES: [("foo", Foo)]}
         assert home_view(request) ==\
             {'dashboard_row_len': 3, 'resources': [("foo", Foo)]}
+
+    def test_unicode_group_name(self):
+        request = testing.DummyRequest()
+        request.registry.settings = {CONFIG_RESOURCES: [("говядо", Foo)]}
+        assert home_view(request) ==\
+            {'dashboard_row_len': 3, 'resources': [("говядо", Foo)]}
+        request.registry.settings = {CONFIG_RESOURCES: [(u"говядо", Foo)]}
+        assert home_view(request) ==\
+            {'dashboard_row_len': 3, 'resources': [(u"говядо", Foo)]}
+        request.registry.settings = {CONFIG_RESOURCES: [
+            (u'\u0433\u043e\u0432\u044f\u0434\u043e', Foo)
+        ]}
+        assert home_view(request) ==\
+            {'dashboard_row_len': 3, 'resources': [(u"говядо", Foo)]}
 
 
 class TestHomeFunc(object):
