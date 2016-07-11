fd155be29be45ad15bf6a2a8867c8316b05d69ca
Svintsov Dmitry <root@uralbash.ru>
2016-07-11 18:46:47 +0500

    clean up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
+from setuptools.command.test import test as TestCommand  # noqa
 
 here = os.path.dirname(os.path.realpath(__file__))
 
